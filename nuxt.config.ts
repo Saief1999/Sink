5d7d510b5914162c419d42e68909cef013165efa
Saief Zneti <zneti@saief.me>
2026-07-16 21:20:21 +0200

    fix: Reverted small change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
     classSuffix: '',
   },
   runtimeConfig: {
-    siteToken: process.env.NUXT_SITE_TOKEN || 'aaaaaaaa',
+    siteToken: process.env.NUXT_SITE_TOKEN || crypto.randomUUID(),
     cfAccessTeamDomain: '',
     cfAccessAud: '',
     redirectStatusCode: '301',
